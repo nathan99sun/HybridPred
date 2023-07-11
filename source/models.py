3bde9259140845d1ab46775438e05c85f8413d87
Daniel <cdn27@cam.ac.uk>
2023-07-11 10:44:29 -0700

    Implemented multi-stage training

--- source/models.py
+++ source/models.py
@@ -3,12 +3,11 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
 class AutoEncoder_ElasticNet(nn.Module):
-    def __init__(self, n_features, n_cycles=49, alpha=0.5, en_lambda = 0.1, lr = 1e-3, epochs = 500, batch_size=16):
+    def __init__(self, n_features, n_cycles=49, alpha=0.5, lr = 1e-3, epochs = 500, batch_size=16):
         super(AutoEncoder_ElasticNet, self).__init__()
 
         self.alpha = alpha
         self.n_features = n_features
-        self.en_lambda = en_lambda
         self.lr = lr
         self.epochs = epochs
         self.batch_size = batch_size
@@ -48,33 +47,44 @@ class AutoEncoder_ElasticNet(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
     
-    def fit(self, train_data, lr = None, epochs = None, batch_size = None, log_loss=False, prediction_weight = 0.1, decoding_weight = 1):
 
-        # Use this to overwrite the lr and epochs defined with the model
-        if lr is None: lr = self.lr
-        if epochs is None: epochs = self.epochs
-        if batch_size is None: batch_size = self.batch_size
+    def fit(self, train_data, train_policy, verbose = True, log_loss = False):
 
-        optimiser = torch.optim.Adam(self.parameters(), lr=lr)
         self.train()
         loss_function = nn.MSELoss()
-        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-        for ep in range(epochs):
-            for batch in train_loader:
-                optimiser.zero_grad()
-                train_inputs, train_labels = batch
+        num_stages = train_policy["num_stages"]
+        for stage in range(num_stages):
 
-                outputs = self.forward(train_inputs)
-                predictions = self.elastic_net_predict(train_inputs)
-                if log_loss:
-                    loss = loss_function(train_labels, predictions[:, 0])*prediction_weight + loss_function(train_inputs, outputs)*decoding_weight
-                else:
-                    loss = loss_function(10**train_labels, 10**predictions[:, 0])*prediction_weight + loss_function(train_inputs, outputs)*decoding_weight
-                loss += self.elastic_net_loss()*self.en_lambda
+            if verbose: print("\nStage {}:\n".format(stage+1))
 
-                loss.backward()
-                optimiser.step()
-                
-            if ep % int(epochs / 10) == 0:
-                print(f"Epoch {ep}/{epochs}, loss: {loss.item():.2f}")
+            epochs = train_policy["epochs"][stage]
+            lr = train_policy["learning_rates"][stage]
+            batch_size = train_policy["batch_sizes"][stage]
+            prediction_weight = train_policy["prediction_weights"][stage]
+            decoding_weight = train_policy["decoding_weights"][stage]
+            en_weight = train_policy["decoding_weights"][stage]
+
+            train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+            optimiser = torch.optim.Adam(self.parameters(), lr=lr)
+
+            for ep in range(epochs):
+                for batch in train_loader:
+
+                    train_inputs, train_labels = batch
+                    outputs = self.forward(train_inputs)
+                    predictions = self.elastic_net_predict(train_inputs)
+
+                    if log_loss:
+                        loss = loss_function(train_labels, predictions[:, 0])*prediction_weight 
+                    else:
+                        loss = loss_function(10**train_labels, 10**predictions[:, 0])*prediction_weight
+                    loss += self.elastic_net_loss()*en_weight + loss_function(train_inputs, outputs)*decoding_weight
+
+                    optimiser.zero_grad()
+                    loss.backward()
+                    optimiser.step()
+
+                if verbose:    
+                    if (ep+1) % int(epochs / 10) == 0: print(f"Epoch {ep+1}/{epochs}, loss: {loss.item():.2f}")
+
